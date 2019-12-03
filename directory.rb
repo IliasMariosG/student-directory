74ecbe1efa813875605b29eae086661e868b9dcc
Ilias Marios Grigoropoulos <ilias_grigorop2@hotmail.com>
2019-12-03 16:43:03 +0000

    Added methods

--- directory.rb
+++ directory.rb
@@ -2,14 +2,27 @@
 students = [ "Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", 
            "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",
            "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon",
-           "Norman Bates"]
+           "Norman Bates" ]
+
+# method definition
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
 # print students
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print(names)
+  names.each do |student|
+    puts student
+  end
 end
 
 # print the total number of students
-puts "Overall, we have #{students.count} great students"
+# footer method definition
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# call the methods
+print_header
+print(students)
+print_footer(students)
